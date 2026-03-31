759b7bbcda1418673c44941c4102689465f22be1
mikaballz <b2029milmapalo@gmail.com>
2026-03-31 20:48:18 +0800

    yessir

--- thirdgraded/script.js
+++ thirdgraded/script.js
@@ -1,26 +1,30 @@
-function plotPoint() {
-    console.log(x0, y0, x, y) // checks the arguments passed to this function
-    in1.innerHTML = x0 + " " + y0 + " " + x + " " + y;
+function plotPoint(x0, y0, x, y) {
+    console.log(x0, y0, x, y);
+
+    document.getElementById("in1").innerHTML =
+        x0 + " " + y0 + " " + x + " " + y;
+
     var point = document.createElement('div');  
     point.className = 'point';
-    /* control where to place the div on the screen using left and bottom of position:absolute */
-    point.styleleft = (x - x0 + 200 - 5) + 'px'; // subtract half the width of the point to center it by changing left css property
-    point.stylebottom = (y - y0 + 200 - 5) + 'px'; // subtract half the height of the point to center it by changing bottom css property
+
+    point.style.left = (x - x0 + 200 - 5) + 'px';
+    point.style.bottom = (y - y0 + 200 - 5) + 'px';
+
     document.getElementById('coordinatePlane').appendChild(point);
 
     if (x > x0 && y > y0){
-      out1.innerHTML = "NE"
+        document.getElementById("out1").innerHTML = "NE";
     }
     else if (x < x0 && y < y0){
-      out1.innerHTML = "SO"
+        document.getElementById("out1").innerHTML = "SO";
     }
     else if (x > x0 && y < y0){
-      out1.innerHTML = "SE"
+        document.getElementById("out1").innerHTML = "SE";
     }
     else if (x < x0 && y > y0){
-      out1.innerHTML = "NO"
+        document.getElementById("out1").innerHTML = "NO";
     }
     else{
-      out1.innerHTML = "divisa"
+        document.getElementById("out1").innerHTML = "divisa";
     }
-  }
+}
